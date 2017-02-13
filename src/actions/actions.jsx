3a83ae374820398313bdcb799a314ffb819d7f2b
Vadim Postanovkin <v.postanovkin@gmail.com>
2017-02-13 14:45:49 +0300

    Add actions

---
+++ src/actions/actions.jsx
@@ -0,0 +1,30 @@
+let setSearchText = searchText => {
+    return {
+        type: 'SET_SEARCH_TEXT',
+        searchText
+    };
+};
+let addTodo = (text) => {
+    return {
+        type: 'ADD_TODO',
+        text
+    };
+};
+let toggleShowCompleted = () => {
+    return {
+        type: 'TOGGLE_SHOW_COMPLETED'
+    };
+};
+let toggleTodo = id => {
+    return {
+        type: 'TOGGLE_TODO',
+        id
+    };
+};
+
+export let actions = {
+    setSearchText,
+    addTodo,
+    toggleShowCompleted,
+    toggleTodo
+};
---
+++ src/tests/actions/actions.test.jsx
@@ -0,0 +1,45 @@
+import expect from 'expect';
+import { actions } from '../../actions/actions.jsx';
+
+console.log(actions);
+
+describe('Actions', () => {
+    it('should generate search text action', () => {
+        let action = {
+            type: 'SET_SEARCH_TEXT',
+            searchText: 'Test Test'
+        };
+        let res = actions.setSearchText(action.searchText);
+        
+        expect(res).toEqual(action);
+    });
+
+    it('should generate add todo action', () => {
+        let action = {
+            type: 'ADD_TODO',
+            text: 'Test Test'
+        };
+        let res = actions.addTodo(action.text);
+        
+        expect(res).toEqual(action);
+    });
+
+    it('should generate toggle show completed action', () => {
+        let action = {
+            type: 'TOGGLE_SHOW_COMPLETED'
+        };
+        let res = actions.toggleShowCompleted();
+        
+        expect(res).toEqual(action);
+    });
+
+    it('should generate toggle todo action', () => {
+        let action = {
+            type: 'TOGGLE_TODO',
+            id: 123
+        };
+        let res = actions.toggleTodo(123);
+        
+        expect(res).toEqual(action);
+    });
+});
